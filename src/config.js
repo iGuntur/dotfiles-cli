ed5bdd8c4cf46b82f6674ae1a59827e2884df80b
Guntur <poetra.guntur@gmail.com>
2016-09-25 21:47:12 +0700

    base config

---
+++ src/config.js
@@ -0,0 +1,40 @@
+import p from 'path';
+import userHome from 'user-home';
+import globArray from 'glob-array';
+
+class Config {
+
+    /**
+     * Initialize config property
+     *
+     * @return {Object}
+     */
+    constructor() {
+        this.homePath = userHome;
+        this.dotfilesPath = '.dotfiles';
+        this.excludesPath = ['.git', '*.md'];
+    }
+
+    /**
+     * PWD
+     *
+     * @return {String}
+     */
+    pwd() {
+        return p.join(this.homePath, this.excludesPath);
+    }
+
+    /**
+     * Get excludes PATH
+     *
+     * @param  {Array} pattern
+     * @return {Array}
+     */
+    excludes(pattern) {
+        pattern = pattern || this.excludesPath;
+
+        return globArray.sync(pattern, {
+            cwd: this.pwd()
+        });
+    }
+}
